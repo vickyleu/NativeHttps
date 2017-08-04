6c2c9968c51d832aaeb499559f46daa8f0a4173f
vicky <vickyleu@foxmail.com>
2017-08-04 19:08:37 +0800

    build protobuf native static library

--- app/src/main/cpp/protobuf/ProtoIO.cpp
+++ app/src/main/cpp/protobuf/ProtoIO.cpp
@@ -7,8 +7,6 @@
 
 using namespace std;
 
-// Main function:  Reads the entire address book from a file and prints all
-//   the information inside.
 int read(User user) {
     // Verify that the version of the library that we linked against is
     // compatible with the version of the headers we compiled against.
@@ -25,9 +23,8 @@ int read(User user) {
     return 0;
 }
 
-// Main function:  Reads the entire address book from a file,
-//   adds one person based on user input, then writes it back out to the same
-//   file.
+
+
 int white(User user) {
     // Verify that the version of the library that we linked against is
     // compatible with the version of the headers we compiled against.
@@ -42,7 +39,7 @@ int white(User user) {
         cerr << "Failed to parse address book." << endl;
         return -1;
     }
-    ZeroCopyOutputStream *zout;
+    ZeroCopyOutputStream *zout=NULL;
     // Write the new address book back to disk.
     CodedOutputStream output(zout, ios::out | ios::trunc | ios::binary);
     if (!user.SerializeToCodedStream(&output)) {
